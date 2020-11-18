ac6f420759db5c35bdc1826651baa122fc69a8e9
Alison Williams <apw5011@gmail.com>
2020-11-18 21:20:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,10 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    BRANDS.each do |brand|
+      if brand != BRANDES[]
+        BRANDS << brand
+      end 
   end
 
   def cobble
